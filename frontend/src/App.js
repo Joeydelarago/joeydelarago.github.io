d642bc5c0dac878e7868236976a3faf9228370f1
Joey de l'Arago <joeydelarago@gmail.com>
2023-04-28 18:48:06 +0200

    Use Routing for dynamic page instead of switch statements.

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,7 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { Component, useState, ListGroup } from "react";
+import React, { Component } from "react";
+import {BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 
 let background = require('./background.js');
 
@@ -14,8 +15,8 @@ const menuItems = [
     link: "/projects",
   },
   {
-    title: "Info",
-    link: "/info",
+    title: "About",
+    link: "/about",
   },
   {
     title: "Contact",
@@ -23,21 +24,21 @@ const menuItems = [
   },
 ]
 
-const projectItems = [
+const projectItems = [ 
+  {
+    title: "GitHub",
+    description: "My programming projects", 
+    link: "https://github.com/Joeydelarago",
+  },
   {
     title: "Pixel Art Meshifier",
-    description: "An application that turns pixel art into meshes",
+    description: "Turn pixel art into meshes",
     link: "https://github.com/Joeydelarago/Pixel-Art-Meshifier",
   },
   {
     title: "Generative Art",
     description: "Art created using p5.js", 
     link: "https://openprocessing.org/user/255301?view=sketches&o=15",
-  },
-  {
-    title: "GitHub",
-    description: "My programming projects", 
-    link: "https://github.com/Joeydelarago",
   },
   {
     title: "3D Models",
@@ -49,9 +50,11 @@ const projectItems = [
 function menuBarItem (item) {
   return (
     <li>
-      <a href={item.link} style={{color: "white"}}>
-        <h2 style={{"font-weight": "bold"}}>{item.title}</h2>
-      </a>
+      <Link to={item.link} style={{color: "white"}}>
+        <h2 style={{"fontWeight": "bold"}}>
+          {item.title}
+        </h2>
+      </Link>
     </li>
   )
 }
@@ -66,13 +69,39 @@ function projectListItem(item) {
   )
 }
 
+function Home() {
+  return;
+}
+
+function Projects() {
+  return (
+    <div className="container-fluid d-flex justify-content-end" style={{marginRight: 35, width: "100%", position: "fixed", bottom: 0, right: 0}}>     
+      <ul className="list-unstyled">
+        {projectItems.map((item) => projectListItem(item))}
+      </ul>
+    </div>
+  )
+}
+
+function About() {
+  return (
+    <div class="container-fluid d-flex justify-content-end" style={{marginRight: 35, marginLeft: 37, width: "100%"}}>     
+      About
+    </div>
+    );
+}
+
+function Contact() {
+  return <h1>Contact</h1>;
+}
+
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       viewCompleted: false,
-      projectList: projectItems,
       menuList: menuItems,
+      visibleSubmenu: "None"
     };
     
   }
@@ -81,29 +110,28 @@ class App extends Component {
     return this.state.menuList.map((item) => menuBarItem(item))
   }
 
-  renderSubmenu = () => {
-    return this.state.projectList.map((item) => projectListItem(item))
-  }     
-
   render() {
     return (      
     <main> 
-        <script src="background.js"></script>
-        <div class="bg-primary-new text-white p-5 text-left" style={{marginBottom: -10}}>
-          <h1>Joey de l'Arago</h1>
-          <h5>Software Engineer & Creative</h5>
-        </div>
-        
-        <div class="container-fluid" style={{marginLeft: 37}}> 
-          <ul className="list-unstyled">
-            {this.renderMenu()}
-          </ul>
-        </div>
-        <div class="container-fluid d-flex justify-content-end" style={{marginRight: 35, width: "100%", position: "fixed", bottom: 0, right: 0}}>     
-          <ul className="list-unstyled">
-            {this.renderSubmenu()}
-          </ul>
-        </div>
+        <script src="background.js"></script> 
+        <BrowserRouter> 
+          <div class="bg-primary-new text-white p-5 text-left" style={{marginBottom: -10}}>
+            <h1>Joey de l'Arago</h1>
+            <h5>Software Engineer & Creative</h5>
+          </div>
+          
+          <div class="container-fluid" style={{marginLeft: 37}}> 
+            <ul className="list-unstyled">
+              {this.renderMenu()}
+            </ul>
+          </div>
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/projects" element={<Projects/>}/>
+            <Route path="/about" element={<About />} />
+            <Route path="/contact" element={<Contact />} />
+          </Routes>
+        </BrowserRouter> 
       </main>
     )
   }
